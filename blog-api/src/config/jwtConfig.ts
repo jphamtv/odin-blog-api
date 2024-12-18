9df8e0a2d9d3bfe50f797c909dc1eb116ccf94fe
J <jpham26@gmail.com>
2024-12-17 16:42:38 -0800

    Add JWTConfig interface

--- blog-api/src/config/jwtConfig.ts
+++ blog-api/src/config/jwtConfig.ts
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+export interface JWTConfig {
+  secret: string;
+  expiresIn: string | number;
+}
+
 export const jwtConfig = {
-  secret: process.env.JWT_SECRET,
+  secret: process.env.JWT_SECRET || 'your-secret-key',
   expiresIn: '24h',
 };
