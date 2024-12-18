929c9e3a4a68b719089b473081c06c1569d5875b
J <jpham26@gmail.com>
2024-12-18 13:17:17 -0800

    Ensure JWT_SECRET is defined to enhance security

--- blog-api/src/config/jwtConfig.ts
+++ blog-api/src/config/jwtConfig.ts
@@ -2,7 +2,11 @@ import { JWTConfig } from '../types/jwtTypes';
 import dotenv from 'dotenv';
 dotenv.config();
 
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET environment variable is not defined.");
+}
+
 export const jwtConfig: JWTConfig = {
-  secret: process.env.JWT_SECRET || 'your-secret-key',
+  secret: process.env.JWT_SECRET,
   expiresIn: '24h',
 };
