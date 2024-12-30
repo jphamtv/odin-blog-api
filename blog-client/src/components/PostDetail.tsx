431a1428fb7a0d81338bce8ac2ddb4cf766512b3
J <jpham26@gmail.com>
2024-12-30 11:39:18 -0800

    Fix date formatting on posts

--- blog-client/src/components/PostDetail.tsx
+++ blog-client/src/components/PostDetail.tsx
@@ -1,4 +1,5 @@
 import { Post } from '../../../shared/types/postTypes'
+import { formatDate } from '../../../shared/utils/dateUtils';
 
 interface PostDetailProps {
   post: Post
@@ -10,7 +11,7 @@ export default function PostDetail({ post }: PostDetailProps) {
       <h1>{post.title}</h1>
       <div className="flex gap-4 text-sm text-gray-500 mb-8">
         <span>By {post.author.username}</span>
-        <span>{new Date(post.createdAt).toLocaleDateString()}</span>
+        <span>{formatDate(post.createdAt)}</span>
       </div>
       <div className="whitespace-pre-wrap">{post.text}</div>
     </article>
